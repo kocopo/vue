94a61d12fc321fe55d708559068ae2368156a5f3
otakar.kocka <otakar.kocka@bootiq.io>
2022-12-03 17:32:45 +0100

    Cont

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data(){
         return {
-            courseGoal: 'Finish this shit !',
+            courseGoalA: 'Finish this shit !',
+            courseGoalB: 'Master this shit !',
             vueLink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal(){
+            const rnNumber = Math.random();
+            if(rnNumber < 0.5){
+                return this.courseGoalA;
+            }else{
+                return this.courseGoalB;
+            }
+        }
     }
 });
 
