a5f711e53aaf4c22bb78535d2c86d537e2fefc43
otakar.kocka <otakar.kocka@bootiq.io>
2022-12-27 17:02:05 +0100

    attack

---
+++ monster-game/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        };
+    },
+    methods:{
+        attackMonster(){
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            const attackValue = getRandomValue(8, 15);
+            this.monsterHealth -= attackValue;
+        }
+    }
+});
+
+app.mount('#game');
+
+function getRandomValue(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
