cb8850fa2fe66127202bc8b6d1b6dd521b57bf35
Otakar Kočka <o.kocka@okocka.cz>
2023-01-23 12:07:03 +0100

    vue cli intro

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <header><h1>My friends</h1></header>
+        <ul>
+            <friend-contact />
+            <friend-contact />
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0123 456 789',
+                    email: 'manuel@lorenz.com'
+                },
+                {
+                    id: 'ota',
+                    name: 'Otakar Kočka',
+                    phone: '0123 456 78911',
+                    email: 'Otakar@kocka.com'
+                }
+            ]
+        };
+    },
+};
+</script>
+<style lang="">
+    
+</style>
