1f29784e451bb6887a88202186874e59685377fe
Otakar Kočka <o.kocka@okocka.cz>
2023-01-28 16:45:34 +0100

    comm

---
+++ component-communication/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <header><h1>My friends</h1></header>
+        <ul>
+            <friend-contact v-for="friend in friends" :key="friend.id" :name="friend.name" :phone-number="friend.phone" :email-address="friend.email" :is-favourite="true"/>
+            
+        </ul>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import FriendContact from './components/FriendContact.vue';
+
+export default defineComponent({
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel', name: 'Manuel Lorenz', phone: '0123 456 789', email: 'manuel@lorenz.com'
+        },
+        {
+          id: 'ota', name: 'Otakar Kočka', phone: '0123 456 78911', email: 'Otakar@kocka.com'
+        }
+      ]
+    }
+  }
+});
+</script>
+
+<style>
+
+</style>
