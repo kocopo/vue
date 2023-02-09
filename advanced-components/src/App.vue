72e951c25ec1a080572b593829e7ec1eaeb3e2e1
Otakar Kočka <o.kocka@okocka.cz>
2023-02-09 13:37:03 +0100

    advanced components with TS

---
+++ advanced-components/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  <the-header></the-header>
+  <badge-list></badge-list>
+  <user-info
+  :full-name="activeUser.name"
+  :info-text="activeUser.description"
+  :role="activeUser.role"
+  ></user-info>
+
+
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BadgeList from './components/BadgeList.vue';
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BadgeList, TheHeader, UserInfo
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    }
+  },
+});
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ advanced-components/src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>More on Vue Components</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
+
+<style>
+  header {
+    width: 100%;
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #14005e;
+  }
+
+  header h1 {
+    color: white;
+    margin: 0;
+  }
+</style>
