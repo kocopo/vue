2c67caff78dc556cff3c727a2e53ca11afff80ae
Otakar Kočka <o.kocka@okocka.cz>
2023-09-15 18:28:10 +0200

    start vuex

---
+++ vuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <base-container title="Toto"><h1>Hello</h1></base-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BaseContainer from './components/BaseContainer.vue';
+
+export default defineComponent({
+  components: {
+    BaseContainer
+  }
+});
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
